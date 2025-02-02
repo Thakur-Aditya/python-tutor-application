89213df23a94cec83a52e959c432c4840f6a5b38
Thakur-Aditya <87617889+Thakur-Aditya@users.noreply.github.com>
2025-02-03 01:57:04 +0530

    tried to fix production routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material";
 import App from "./App";
 import "./index.css";
@@ -63,7 +67,9 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
+    >
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
